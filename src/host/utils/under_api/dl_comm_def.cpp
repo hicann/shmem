2d7c12ca0feaa48eec173c003c0683ef1f74a951
james88liu <liujianxing1@huawei.com>
2026-03-20 10:29:04 +0800

    add c10 version
    Co-authored-by: james88liu<liujianxing1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !192 merge br_fix_check_version into master
    
    add c10 version
    
    Created-by: james88liu
    Commit-by: james88liu
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/shmem!192

--- src/host/utils/under_api/dl_comm_def.cpp
+++ src/host/utils/under_api/dl_comm_def.cpp
@@ -14,6 +14,8 @@
 #include "dl_comm_def.h"
 #include "utils/shmemi_logger.h"
 
+const std::string DRIVER_VER_V5_BEGIN = "V100R001C10B600";
+const std::string DRIVER_VER_V5_END = "V100R001C10B999";
 const std::string DRIVER_VER_V4 = "V100R001C23SPC005B219";
 const std::string DRIVER_VER_V3 = "V100R001C21B035";
 const std::string DRIVER_VER_V2 = "V100R001C19SPC109B220";
@@ -192,6 +194,11 @@ static bool DriverVersionCheck(const std::string &ver)
 
 int32_t HalGvaPrecheck(void)
 {
+    if (!DriverVersionCheck(DRIVER_VER_V5_END) && DriverVersionCheck(DRIVER_VER_V5_BEGIN)) {
+        SHM_LOG_INFO("Driver version V5 found, compatible with V4");
+        checkVer = HYBM_GVA_V4;
+        return ACLSHMEM_SUCCESS;
+    }
     if (DriverVersionCheck(DRIVER_VER_V4)) {
         SHM_LOG_INFO("Driver version V4 found");
         checkVer = HYBM_GVA_V4;
@@ -213,14 +220,6 @@ int32_t HalGvaPrecheck(void)
         return ACLSHMEM_SUCCESS;
     }
 
-    // 临时check,判断version在区间内
-    std::string verStartA5 = "V100R001C10B600";
-    std::string verEndA5   = "V100R001C10B999";
-    if (!DriverVersionCheck(verEndA5) && DriverVersionCheck(verStartA5)) {
-        SHM_LOG_ERROR("Env is A5, Driver version V4 found");
-        checkVer = HYBM_GVA_V4;
-        return ACLSHMEM_SUCCESS;
-    }
     SHM_LOG_ERROR("Failed to determine driver version");
     return ACLSHMEM_INNER_ERROR;
 }
